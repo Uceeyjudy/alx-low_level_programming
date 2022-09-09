b42ca7ea4e39d817fbab6fbee1d80fec78b0d704
Uceeyjudy <ucjudith@gmail.com>
2022-09-09 09:37:58 -0700

    adding quote

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -2,11 +2,13 @@
 
 /**
  *  main - Entry point
+ *
  *  Return: 1 (success)
  */
 
 int main(void)
 {
-	write(1, "and that piece of art is useful\" -Dora Korpar, 2015-10-19\n",59);
+	char []str1=1, "and that piece of art is useful\" -Dora Korpar, 2015-10-19\n",59;
+	write(2, str1 ,59);
 	return (1);
 }
