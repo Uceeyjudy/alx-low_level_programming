dc424580c4c4620b74742c0897d71409ee3e51be
Uceeyjudy <ucjudith@gmail.com>
2022-09-09 15:27:20 -0700

    printing alphabets

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+/**
+ * main - prints the alphabet in lowercase, and then in uppercase
+ *
+ * Return: Always 0 (success)
+ */
+int main(void)
+{
+	char alpUpper[52] = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYX";
+	int i;
+	for (i = 0; i < 52; i++)
+	{
+		putchar(alpUpper[i]);
+	}
+	putchar('\n');
+	return (0);
+}
