47cb18e318b98605e77abc5791670e4c2953c5a9
Uceeyjudy <ucjudith@gmail.com>
2022-09-11 05:35:08 -0700

    print alphabt

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+/**
+ * main - prints alphabets in lowercase
+ *
+ * Return: 0 always successful
+ */
+int main(void)
+{
+	char alph[24] = "abcdfjhijklmnoprstuvwxyz";
+	int i;
+
+	for (i = 0; i < 24; i++)
+	{
+		putchar(alph[i]);
+	}
+	putchar('\n');
+	return (0);
+}
