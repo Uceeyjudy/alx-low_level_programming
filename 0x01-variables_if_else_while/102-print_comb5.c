e775f26a94b3ee37a96c9102672ab37f43f2dc19
Uceeyjudy <ucjudith@gmail.com>
2022-09-12 08:47:03 -0700

    print combo

---
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -0,0 +1,33 @@
+#include <stdio.h>
+/**
+ * main - prints numbers between 0 and 99
+ *
+ * Return: 0 when successful
+ */
+int main(void)
+{
+	int i, j;
+
+	i = 48;
+	j = 48;
+
+	while (j < 58)
+	{
+		i = 48;
+		while (i < 58)
+		{
+			putchar(j);
+			putchar(i);
+			if (i == 57 && j == 57)
+			{
+				break;
+			}
+			putchar(',');
+			putchar(' ');
+			i++;
+		}
+		j++;
+	}
+	putchar('\n');
+	return (0);
+}
