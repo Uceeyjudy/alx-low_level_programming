d41c1b2d8486e17241558e19a5800ea3d59461e0
Uceeyjudy <ucjudith@gmail.com>
2022-09-12 07:23:33 -0700

    added print reversed alphabet

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+/**
+ * main - prints the lowercase alphabet in reverse, followed by a new line.
+ *
+ * Return: 0 when successful
+ */
+
+int main(void)
+{
+	for (i = 122; i < 96; i++)
+	{
+		putchar(i);
+	}
+	putchar('\n');
+	return (0);
+}
