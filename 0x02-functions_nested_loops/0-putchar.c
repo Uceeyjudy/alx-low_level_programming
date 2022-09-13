bfa9a3af9d6bce6e851a005005ccc5f95b95d4b5
Uceeyjudy <ucjudith@gmail.com>
2022-09-13 07:21:48 -0700

    added putchar

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -3,14 +3,20 @@
 
 /**
  * _putchar - prints _putchar
- * @char - prints the character
- * @c - prints the c
  *
  * Return: 0 if successful
- * -1 if not successful
  */
 
-int _putchar(char, c)
+int _putchar(void)
 {
-	return (write(1, $c, 1));
+	int str[] = {95, 112, 117, 116, 99, 104, 97, 114};
+	int count, sz;
+
+	sz = sizeof(str)/sizeof(int);
+	for (count = 0; count < sz; count++)
+	{
+		_putchar(str[count]);
+	}
+	_putchar('\n');
+	return (0);
 }
