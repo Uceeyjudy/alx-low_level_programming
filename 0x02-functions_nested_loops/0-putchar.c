f8052f1ff14161319844283b2839fc63bffa6c04
Uceeyjudy <ucjudith@gmail.com>
2022-09-13 06:58:27 -0700

    this is wrong

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,8 +1,10 @@
 #include <unistd.h>
-#include <main.h>
+#include "main.h"
 
 /**
  * _putchar - prints _putchar
+ * @char - prints the character
+ * @c - prints the c
  *
  * Return: 0 if successful
  * -1 if not successful
