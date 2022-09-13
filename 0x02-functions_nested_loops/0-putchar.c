16fd6dd19c6525463e7f66dee1d52f6b11aabc75
Uceeyjudy <ucjudith@gmail.com>
2022-09-13 07:29:37 -0700

    added putchar

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -2,12 +2,12 @@
 #include "main.h"
 
 /**
- * _putchar - prints _putchar
+ * main - prints _putchar
  *
  * Return: 0 if successful
  */
 
-int _putchar(void)
+int main(void)
 {
 	int str[] = {95, 112, 117, 116, 99, 104, 97, 114};
 	int count, sz;
