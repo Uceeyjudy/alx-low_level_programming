d07fe496488246adf06cad254ffa2d03a7ed9c11
Uceeyjudy <ucjudith@gmail.com>
2022-09-14 03:48:27 -0700

    checks for absolute value

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,22 @@
+#include "main"
+
+/**
+ * _abs - similar function to abs
+ * computs absolute value of a number
+ *
+ * @i: Ascii value
+ *
+ * Return: absolute value
+ */
+
+int _abs(int i)
+{
+	if (i >= 0)
+	{
+		return (i);
+	}
+	else
+	{
+		return (i * -1);
+	}
+}
