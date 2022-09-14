57f19ff9408bf5acd0e59c91595fd5221a254cb7
Uceeyjudy <ucjudith@gmail.com>
2022-09-14 03:18:39 -0700

    checks for alphabets

---
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -0,0 +1,22 @@
+#include "main.h"
+
+/**
+ * _isalpha - Checks the code if alphabet
+ *
+ * @c: Ascii characters
+ *
+ * Return: 1 if alphabet and 0 for others
+ */
+
+int _isalpha(int c)
+{
+	if ((c >= 65 && c <= 90) || (c >= 97 && c <= 122))
+	{
+		return (1);
+	}
+	else
+	{
+		return (0);
+	}
+	_putchar('\n')
+}
