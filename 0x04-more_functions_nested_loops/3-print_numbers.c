d2831a1a31520be76b53e6d51efbdf0fd66b7086
Uceeyjudy <ucjudith@gmail.com>
2022-09-16 13:00:05 -0700

    added print numbers

---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * main
+ *
+ * Return: 0 when successful
+ */
+
+void print_numbers(void)
+{
+	int i;
+	for (i = 48; i <= 57; i++)
+	{
+		_putchar(i);
+	}
+	_putchar('\n')
+}
