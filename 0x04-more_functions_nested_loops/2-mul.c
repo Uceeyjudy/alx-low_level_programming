874375def49a684ad390722eeee064ee96fb9333
Uceeyjudy <ucjudith@gmail.com>
2022-09-16 12:47:59 -0700

    multipying a and b

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,14 @@
+#include "main.h"
+
+/**
+ * mul - function to multiply a and b
+ *
+ * @a: first num to be multiplied
+ * @b: second num to be multiplied
+ * Return: the multiplication
+ */
+
+int mul(int a, int b)
+{
+	return (a * b);
+}
