581936c19021802c6cd6dd44c8611ad3171ad020
Uceeyjudy <ucjudith@gmail.com>
2022-09-17 00:57:06 -0700

    added fizz buzz

--- 0x04-more_functions_nested_loops/9-fizz_buzz.c
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -16,23 +16,23 @@ int main(void)
 	i = 1;
 	printf("%d", i);
 
-	for (i = 0; i < 100; i++)
+	for (i = 2; i <= 100; i++)
 	{
 		if ((i % 15) == 0)
 		{
-			printf("FizzBuzz");
+			printf(" FizzBuzz");
 		}
 		else if ((i % 5) == 0)
 		{
-			printf("Buzz");
+			printf(" Buzz");
 		}
 		else if ((i % 3) == 0)
 		{
-			printf("Fizz");
+			printf(" Fizz");
 		}
 		else
 		{
-			printf("%d", i);
+			printf(" %d", i);
 		}
 	}
 	printf("\n");
