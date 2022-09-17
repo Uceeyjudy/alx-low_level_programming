13313829478e766c0dbaf67879cbf6f2ab3133b0
Uceeyjudy <ucjudith@gmail.com>
2022-09-16 22:25:33 -0700

    added more numbers

--- 0x04-more_functions_nested_loops/5-more_numbers.c
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -14,14 +14,11 @@ void more_numbers(void)
 	for (i = 0; i < 10; i++)
 	{
 		for (j = 0; j < 15; j++)
+		{
 			if (j >= 10)
-			{
 				_putchar((j / 10) + 48);
-			}
-			else
-			{
-				_putchar((j % 10) + 48);
-			}
+			_putchar((j % 10) + 48);
+		}
 		_putchar('\n');
 	}
 }
