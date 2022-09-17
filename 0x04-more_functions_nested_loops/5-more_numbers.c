9efbca045e0b9456933b016d95b49719bbb6ce62
Uceeyjudy <ucjudith@gmail.com>
2022-09-16 22:18:55 -0700

    added print more numbers

---
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -0,0 +1,27 @@
+#include "main.h"
+
+/**
+ * more_numbers - prints numbers 1 to 14
+ * ten times
+ *
+ * Return: does not return
+ */
+
+void more_numbers(void)
+{
+	int i, j;
+
+	for (i = 0; i < 10; i++)
+	{
+		for (j = 0; j < 15; j++)
+			if (j >= 10)
+			{
+				_putchar((j / 10) + 48);
+			}
+			else
+			{
+				_putchar((j % 10) + 48);
+			}
+		_putchar('\n');
+	}
+}
