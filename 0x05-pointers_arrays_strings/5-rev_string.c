2c716bc99c83f857f8f5f14ebe953542d165f3cb
Uceeyjudy <ucjudith@gmail.com>
2022-09-19 06:18:52 -0700

    added rev-string

---
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -0,0 +1,30 @@
+#include "main.h"
+
+/**
+ * rev_string - reverses a string
+ * @s: input string
+ * Return: does not retrun anything
+ */
+void rev_string(char *s)
+{
+	int count = 0, a, b;
+	char *str, rev;
+
+	while (count >= 0)
+	{
+		if (str[count] == '\0')
+			break;
+		count++;
+	}
+	str = s;
+
+	for (a = 0; a < (count - 1); a++)
+	{
+		for (b = a + 1; b > 0; b--)
+		{
+			rev = *(str + b);
+			*(str + b) = *(str + (b - 1));
+			*(str + (b - 1)) = rev;
+		}
+	}
+}
