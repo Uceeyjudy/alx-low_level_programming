163ab2b7e773677e01373355c3c1b6e2c0fd36f6
Uceeyjudy <ucjudith@gmail.com>
2022-09-19 04:17:31 -0700

    added 1-swap

---
+++ 0x05-pointers_arrays_strings/1-swap.c
@@ -0,0 +1,14 @@
+#indlude "main.h"
+/**
+ * swap_int - swappes the value of intergers
+ * @a: first interger
+ * @b: seconde interger
+ * Return: no return value
+ */
+
+void swap_int(int *a, int *b)
+{
+	int c = *a;
+	*a = *b;
+	*b = c;
+}
