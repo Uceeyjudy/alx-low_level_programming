fdb72d2b8412ec24accb6c868cdd143544ce162f
Uceeyjudy <ucjudith@gmail.com>
2022-09-21 06:58:58 -0700

    added pointer and array and strings

--- 0x06-pointers_arrays_strings/0-strcat.c
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -1,25 +1,25 @@
 #include "main.h"
-
 /**
- * _strcat - concantenates two string
- * @dest: destination
- * @src: source
- * Return: pointer to the destination
+ * _strcat - concatenates two strings,
+ * @dest: destination.
+ * @src: source.
+ * Return: the pointer to dest.
  */
 char *_strcat(char *dest, char *src)
 {
-	int count1 = 0, count2 = 0;
+	int count = 0, count2 = 0;
 
-	while (*(dest + count1) != '\0')
+	while (*(dest + count) != '\0')
 	{
-		count1++;
+		count++;
 	}
+
 	while (count2 >= 0)
 	{
-		*(dest + count1) = *(src + count2);
+		*(dest + count) = *(src + count2);
 		if (*(src + count2) == '\0')
-				break;
-		count1++;
+			break;
+		count++;
 		count2++;
 	}
 	return (dest);
