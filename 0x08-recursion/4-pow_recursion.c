b1cbc5aece41e990bbd5df21382a2a5f2931e38b
Uceeyjudy <ucjudith@gmail.com>
2022-09-28 08:37:17 -0700

    added pow recursion

--- 0x08-recursion/4-pow_recursion.c
+++ 0x08-recursion/4-pow_recursion.c
@@ -1,16 +1,16 @@
 #include "main.h"
 /**
- * _power_recursion - returns the value of x raised to y
+ * _pow_recursion - returns the value of x raised to y
  * @x: base
  * @y: power
  * Return: value of the exponetiation
  */
-int _power_recursion(int x, int y)
+int _pow_recursion(int x, int y)
 {
 	if (y < 0)
 		return (-1);
 	else if (y == 0)
 		return (1);
 	else
-		return (x * _power_recursion(x, y - 1));
+		return (x * _pow_recursion(x, y - 1));
 }
