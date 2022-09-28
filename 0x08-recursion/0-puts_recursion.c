0dfff878fee24255c4d89d9701421affdd00969d
Uceeyjudy <ucjudith@gmail.com>
2022-09-28 06:09:25 -0700

    puts recursion and readme file

---
+++ 0x08-recursion/0-puts_recursion.c
@@ -0,0 +1,17 @@
+#include "main.h"
+/**
+ * _puts_recursion - returns a string passed
+ * @s: the string passed
+ * Return: nothing
+ */
+
+void _puts_recursion(char *s)
+{
+	if (*s != '\0')
+	{
+		_putchar(*s);
+		_puts_recursion(s + 1);
+	}
+	else
+		_putchar('\n');
+}
