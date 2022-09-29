7c08adb7f0fb9e09578274b5d9a7ad179a44ba8d
Uceeyjudy <ucjudith@gmail.com>
2022-09-29 04:23:41 -0700

    added palindrom

--- 0x08-recursion/100-is_palindrome.c
+++ 0x08-recursion/100-is_palindrome.c
@@ -19,7 +19,7 @@ int _strlen_recursion(char *s)
  * @n2: biggest iterator
  * Return: .
  */
-int comaprator(char *s, int n1, int n2)
+int comparator(char *s, int n1, int n2)
 {
 	if (*(s + n1) == *(s + n2))
 	{
